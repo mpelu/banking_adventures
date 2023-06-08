c879c4e4420e3935c66a98857cdfe40e6ca8907b
mpelu <pelu0002@algonquinlive.com>
2023-06-08 15:07:25 -0400

    renaming; add transaction breakdown

--- fbar.py
+++ fbar.py
@@ -5,35 +5,37 @@ class AccountDF:
     def __init__(self, name, file_name):
         self.name = name + ' Account'
         self.file_name = file_name
-        self.data_frame = pd.read_csv(file_name) 
-
+        self.df = pd.read_csv(file_name) # DataFrame object
 
     # basic functions
 
     def head(self):
-        print(self.data_frame.head())
+        print(self.df.head())
 
     def file_metadata(self):
-        print(self.data_frame.info())
+        print(self.df.info())       
 
-  
+    # TODO check for more elegant solution
+    def transaction_breakdown(self):
+        truncated_df = self.df.iloc[:, [2, 3, 4]]
+        truncated_df.columns = ['Withdrawals', 
+                                'Deposits', 
+                                'Total Transactions']
+        row_count = truncated_df.count() # Series object
+        print(row_count)
+
+
     # fbar functions
 
-    # TODO:
-    # specify date range
-    # add currency sign
     def max_balance(self):
-        print('Highest balance of', self.name , ': ' , self.data_frame['Balance'].max()) 
+        print('Highest balance of', self.name , ': ' , self.df['Balance'].max()) 
 
-    # TODO:
-    # str.contains regex
-    # add currency sign
-    def sum_income(self):
-        df = self.data_frame
-        filtered_df = df[df['Transaction_Desc'].str.contains("INTEREST CREDIT")]
+    def total_income(self):
+        filtered_df = self.df[self.df['Transaction_Desc'].str.contains("INTEREST CREDIT")]
         total_income = filtered_df['Deposits'].sum()
         print(total_income)
 
+        
 ch = AccountDF('Checking', 'checking.csv')
 ch.max_balance()
 ch.file_metadata()
@@ -41,4 +43,8 @@ ch.file_metadata()
 sa = AccountDF('Savings', 'savings.csv')
 sa.max_balance()
 sa.file_metadata()
-sa.sum_income()
+sa.total_income()
+sa.head()
+sa.transaction_breakdown()
+
+
